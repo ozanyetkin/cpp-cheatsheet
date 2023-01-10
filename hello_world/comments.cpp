a9c15e819dad8b8c1ac6a38aaecc727f26c9f5c8
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2023-01-10 15:21:36 +0300

    hello world finished, variables next

---
+++ hello_world/comments.cpp
@@ -0,0 +1,12 @@
+// Harry Potter code which can be compiled with g++ comments.cpp and run with ./comments from the terminal
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "Expecto Patronum\n";
+    /*
+    This is for multiline comments
+    You can write whatever you want in between
+    */
+}
---
+++ hello_world/initials.cpp
@@ -0,0 +1,13 @@
+// Fun fact: My initials are pronounced as "Oh yeah!"
+
+#include <iostream>
+
+int main()
+{
+    // Your program lives here
+    std::cout << " OOO  Y   Y\n";
+    std::cout << "O   O  Y Y \n";
+    std::cout << "O   O   Y  \n";
+    std::cout << "O   O   Y  \n";
+    std::cout << " OOO    Y  \n";
+}
